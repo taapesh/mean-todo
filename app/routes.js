b3a71b5b206ab464a9a645ef93a69f8eae534d9e
taapesh <starmansix@gmail.com>
2016-03-01 11:23:00 -0600

    Modified file structure

---
+++ app/routes.js
@@ -0,0 +1,46 @@
+var Todo = require('./models/Todo');
+
+module.exports = function(app) {
+    
+    // API route to get all todos
+    app.get('/api/todos', function(req, res) {
+        Todo.find(function(err, todos) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(todos);
+        });
+    });
+
+    // API route to create a new todo
+    app.post('/api/todos', function(req, res) {
+        Todo.create({
+            text: req.body.text,
+            done: false
+        }, function(err, todo) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(todo);
+
+
+        });
+    });
+
+    // API route to delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id: req.params.todo_id
+        }, function(err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({'message': 'Deleted todo'});
+        });
+    });
+
+    // Define single route for our single page app, angular handles the rest
+    app.get('*', function(req, res) {
+        res.sendFile('./public/index.html');
+    });
+};
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var bodyParser  = require('body-parser');
 var methodOverride = require('method-override');
 
 // Connect to db
-mongoose.connect('mongodb://localhost/mean-project');
+var database = require('./config/database');
+mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -15,54 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// Define model todo
-var Todo = mongoose.model('Todo', {
-    text: String
-});
-
-// App routes
-
-// API route to get all todos
-app.get('/api/todos', function(req, res) {
-    Todo.find(function(err, todos) {
-        if (err) {
-            res.send(err);
-        }
-        res.json(todos);
-    });
-});
-
-// API route to create a new todo
-app.post('/api/todos', function(req, res) {
-    Todo.create({
-        text: req.body.text,
-        done: false
-    }, function(err, todo) {
-        if (err) {
-            res.send(err);
-        }
-        res.json(todo);
-
-
-    });
-});
-
-// API route to delete a todo
-app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-        _id: req.params.todo_id
-    }, function(err) {
-        if (err) {
-            res.send(err);
-        }
-        res.json({'message': 'Deleted todo'});
-    });
-});
-
-// Define single route for our single page app, angular handles the rest
-app.get('*', function(req, res) {
-    res.sendFile('./public/index.html');
-});
+// routes
+require('./app/routes')(app);
 
 // Start listening for requests
 app.listen(3000);
